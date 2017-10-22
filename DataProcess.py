3e5db2cf29248e99cfe72360c604c1b15166801a
Vo4f <vo4f@outlook.com>
2017-10-22 20:41:49 +0800

    Initial commit

---
+++ DataProcess.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@author: vo4f
+@project: PyCharm
+@file: DataProcess.py
+@time: 2017/10/22 20:25
+@doc: Process basic data
+"""
+class DataProcess:
+
+    def __init__(self, flag):
+        self.flag = flag
